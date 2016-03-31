18021e4932846ec042a44f818782a821acf68b14
Tim Anema <timanema@gmail.com>
2016-03-31 17:37:26 -0400

    Removed request for feedback on GetIntegerv that has been proven to work in the webgl wrapper

--- gl_webgl.go
+++ gl_webgl.go
@@ -297,7 +297,6 @@ func GetFloatv(dst []float32, pname Enum) {
 }
 
 func GetIntegerv(pname Enum, data []int32) {
-	println("GetIntegerv: not yet tested (TODO: remove this after it's confirmed to work. Your feedback is welcome.)")
 	result := c.Call("getParameter", pname)
 	length := result.Length()
 	for i := 0; i < length; i++ {
